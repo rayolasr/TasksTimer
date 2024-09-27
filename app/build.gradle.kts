a6f71d081ddcc10fdbae9f828999c3f93c637c81
Carlos Gaze Martín <gazemartincarlos@gmail.com>
2024-09-27 19:25:51 +0100

    feature: show current date above tasks list

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,12 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.viewpager2)
+    implementation(libs.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
